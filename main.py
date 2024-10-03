a475058e186f0aeab1272a3bcf09f5854e4d74de
Максим <maximers6e@gmail.com>
2024-10-03 09:55:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-number = input("Введите восьмизначное число: ")
-if len(number) == 8 and number.isdigit():
-    result = sum(int(digit) for digit in number)
-    print(f"Сумма цифр числа {number} равна {result}")
-else:
-    print("Пожалуйста, введите корректное восьмизначное число.")
+n = 12345678  # Пример числа
+a = n // 10000000 # целочис деление
+b = (n // 1000000) % 10
+c = (n // 100000) % 10
+d = (n // 10000) % 10
+e = (n // 1000) % 10
+f = (n // 100) % 10
+g = (n // 10) % 10
+h = n % 10
+
+sum = a + b + c + d + e + f + g + h #сумма всеех эл-тов
+print(sum)
